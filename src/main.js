ae42805783914276c6224b8d976bb92c89337d08
XinZhu <chenxinzhu.xz@bytedance.com>
2021-03-07 03:13:01 +0800

    fix: some

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'animate.css';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import { Button, Tag } from 'element-ui';
+
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI);
+Vue.use(Button);
+
+Vue.use(Tag);
+
 
 
 new Vue({
